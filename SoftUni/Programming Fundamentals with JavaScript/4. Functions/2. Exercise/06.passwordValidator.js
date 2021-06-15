61065c2d701f023d0d2443ac544bdc069292052e
mitach <malinovd02@gmail.com>
2021-06-15 20:39:16 +0300

    Add homework

---
+++ SoftUni/Programming Fundamentals with JavaScript/4. Functions/2. Exercise/06.passwordValidator.js
@@ -0,0 +1,71 @@
+function passwordValidator(password) {
+
+    let isLengthValid = isBetweenSixAndTen(password);
+    let consistsLettersAndDigits = consistOnlyLettersAndDigits(password);
+    let containsAtLeastTwoDigits = hasAtLeastTwoDigits(password);
+
+    if (isLengthValid && consistsLettersAndDigits && containsAtLeastTwoDigits) {
+        console.log('Password is valid');
+        return;
+    }
+
+    if (!isLengthValid) {
+        console.log('Password must be between 6 and 10 characters');
+    }
+
+    if (!containsAtLeastTwoDigits) {
+        console.log('Password must consist only letters and digits');
+    }
+
+    if (!consistsLettersAndDigits) {
+        console.log('Password must have at least 2 digits');
+    }
+
+    function isBetweenSixAndTen(string) {
+        if (string.length >= 6 && string.length <= 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function consistOnlyLettersAndDigits(string) {
+        for (let char of string) {
+            let charCode = char.charCodeAt(0);
+
+            if (!(charCode >= 48 && charCode <= 57) &&
+                !(charCode >= 65 && charCode <= 90) &&
+                !(charCode >= 97 && charCode <= 122)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    function hasAtLeastTwoDigits(string) {
+        let counter = 0;
+
+        for (let char of string) {
+            let charCode = char.charCodeAt(0);
+
+            if (charCode >= 48 && charCode <= 57) {
+                counter++;
+            }
+        }
+
+        // return counter >= 2;
+
+        if (counter >= 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+passwordValidator('logIn');
+passwordValidator('MyPass123');
+passwordValidator('Pa$s$s');
