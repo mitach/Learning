54bf432f8f9481ff6a63471438a58d0f6730c4ca
mitach <malinovd02@gmail.com>
2021-06-17 18:17:39 +0300

    Add homework

---
+++ SoftUni/Programming Fundamentals with JavaScript/5. Arrays Advanced/1. Lab/04.LastKSequence.js
@@ -0,0 +1,17 @@
+function lastKSequence(n, k) {
+    let result = [1];
+
+    for (let i = 1; i < n; i++) {
+        let elements = result.slice(-k);
+        let current = 0;
+        for (let num of elements) {
+            current += num
+        }
+
+        result.push(current);
+    }
+
+    console.log(result.join(' '));
+}
+
+lastKSequence(6, 3);
