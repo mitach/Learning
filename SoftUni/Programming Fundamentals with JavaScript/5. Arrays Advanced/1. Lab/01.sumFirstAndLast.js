963642782a0fb4d6b2731d25c0ee8e1f29690a7c
mitach <malinovd02@gmail.com>
2021-06-20 23:07:53 +0300

    change

--- SoftUni/Programming Fundamentals with JavaScript/5. Arrays Advanced/1. Lab/01.sumFirstAndLast.js
+++ SoftUni/Programming Fundamentals with JavaScript/5. Arrays Advanced/1. Lab/01.sumFirstAndLast.js
@@ -1,6 +1,7 @@
 function sumFirstAndLast(numsAsStringsArr) {
+    //initiation
     let first = Number(numsAsStringsArr.shift());
     let last = Number(numsAsStringsArr.pop());
-
+    //result
     return first + last;
 }
