b7d55e5504602ca76b3bb6c222fd9e6c5d6b7fc9
mitach <malinovd02@gmail.com>
2021-06-21 23:55:20 +0300

    Add homework (TeachMe)

---
+++ TeachMe/Programming for beginners - JavaScript/01. Introduction/02.dataTypes.js
@@ -0,0 +1,9 @@
+function dataTypes() {
+    let num = 100;
+    console.log(num);
+
+    let num2 = 3.15;
+    console.log(num2);
+}
+
+dataTypes();
---
+++ TeachMe/Programming for beginners - JavaScript/01. Introduction/03.readParameter.js
@@ -0,0 +1,9 @@
+function solve(name) {
+    console.log(name);
+
+    // or - return name
+}
+
+solve("Ivan");
+solve("Andrey");
+solve("Maria");
