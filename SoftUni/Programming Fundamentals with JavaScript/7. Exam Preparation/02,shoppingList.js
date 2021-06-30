56cc4f2f63aee2cefa1e1d29cb49a040a874815b
mitach <malinovd02@gmail.com>
2021-06-30 22:24:02 +0300

    Add homework (SoftUni)
    fix problem 02 later

---
+++ SoftUni/Programming Fundamentals with JavaScript/7. Exam Preparation/02,shoppingList.js
@@ -0,0 +1,60 @@
+function shoppingList(input) {
+    let groceries = input.shift().split('!');
+
+    let line = input.shift();
+
+    while (line != 'Go Shopping!') {
+        let tokens = line.split(' ')
+        let [command, product, newProduct] = [tokens[0], tokens[1], tokens[2]];
+
+        switch (command) {
+            case 'Urgent':
+                if (!(groceries.includes(product))) {
+                    groceries.unshift(product);
+                }
+
+                break;
+            case 'Unnecessary':
+                if (groceries.includes(product)) {
+                    let index = groceries.indexOf(product);
+                    groceries.splice(index, 1)
+                }
+
+                break;
+            case 'Correct':
+                if (groceries.includes(product)) {
+                    let index = groceries.indexOf(product);
+                    groceries.splice(index, 1, newProduct);
+                }
+
+                break;
+            case 'Rearrange':
+                if (groceries.includes(product)) {
+                    let index = groceries.indexOf(product);
+                    groceries.splice(index, 1);
+                    groceries.unshift(product);
+                }
+
+                break;
+
+        }
+
+        line = input.shift();
+    }
+
+    console.log(groceries.join(', '));
+}
+
+shoppingList(['Tomatoes!Potatoes!Bread!Milk',
+    'Unnecessary Milk',
+    'Urgent Tomatoes',
+    'Go Shopping!']);
+
+shoppingList([
+    'Milk!Pepper!Salt!Water!Banana',
+    'Urgent Salt',
+    'Unnecessary Milk',
+    'Correct Pepper Onion',
+    'Rearrange Water',
+    'Correct Banana Potatoes',
+    'Go Shopping!']);
