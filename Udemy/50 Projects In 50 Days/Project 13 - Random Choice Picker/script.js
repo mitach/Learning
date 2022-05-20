3474942864ea24a9b0fc8c89341585dbda6fe421
mitach <malinovd02@gmail.com>
2022-05-20 16:54:41 +0300

    feat: select random tag

--- Udemy/50 Projects In 50 Days/Project 13 - Random Choice Picker/script.js
+++ Udemy/50 Projects In 50 Days/Project 13 - Random Choice Picker/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value);
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10);
+
+        randomSelect();
+    }
 })
 
 function createTags(input) {
@@ -20,4 +28,40 @@ function createTags(input) {
         tagEl.textContent = tag;
         tagsEl.appendChild(tagEl)
     });
+}
+
+function randomSelect() {
+    const times = 20;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unhighlightTag(randomTag )
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100)
+    }, times * 100)     
+}
+
+function pickRandomTag()  {
+    const tags = document.querySelectorAll('.tag');
+
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight');
 }
